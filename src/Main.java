6223fd978af24d202cf7d7840411ad2542203c48
YJ <1262969738@qq.com>
2023-12-14 15:16:45 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args){
+        Integer i = new Integer(10); // 作为包装类的int
+        System.out.println(i);
+    }
+}
